b31d510002d4d8452b504821f7e0da2b4252b5d5
javiercplus <javiercplus@noreply.codeberg.org>
2026-02-08 01:22:14 +0100

    Actualizar installer.c

--- installer.c
+++ installer.c
@@ -1,6 +1,6 @@
 /*
  * installer.c
- * FULL LOGIC: Replicating original void-installer steps
+ * LOCAL/LIVE INSTALLATION MODE (copy_rootfs + cleanup)
  */
 #include "neko_installer.h"
 #include <sys/mount.h>
@@ -48,8 +48,7 @@ int run_sync(AppData *app, const char *fmt, ...) {
     va_end(args);
     
     log_to_ui(app, cmd, -1.0);
-    int status = system(cmd);
-    return WEXITSTATUS(status);
+    return system(cmd);
 }
 
 gpointer install_thread(gpointer data) {
@@ -59,7 +58,7 @@ gpointer install_thread(gpointer data) {
     char efi_part[64];
     const char *TARGETDIR = "/mnt/target";
     
-    // 1. Retrieve data from UI
+    // 1. Retrieve data
     const gchar *disk_name = app->selected_disk;
     if (!disk_name) {
         log_to_ui(app, "CRITICAL ERROR: No disk selected.", 0.0);
@@ -89,92 +88,104 @@ gpointer install_thread(gpointer data) {
 
     if (!root_pass || strlen(root_pass) < 1) { log_to_ui(app, "Error: Root password missing.", 0.0); app->installing = FALSE; return NULL; }
     
-    log_to_ui(app, "--- STARTING NEKO VOID INSTALLATION ---", 0.1);
+    log_to_ui(app, "--- STARTING LOCAL INSTALLATION (Live Copy) ---", 0.1);
 
-    // 2. Formatting & Mounting
-    log_to_ui(app, "Formatting root partition...", 0.2);
-    if (run_sync(app, "mkfs.ext4 -F %s", root_part) != 0) {
-        log_to_ui(app, "ERROR formatting root.", 0.0); app->installing = FALSE; return NULL;
-    }
-
+    // 2. Mount Target
+    log_to_ui(app, "Formatting and mounting target partitions...", 0.2);
+    run_sync(app, "mkfs.ext4 -F %s", root_part);
     run_sync(app, "mkdir -p %s", TARGETDIR);
-    if (mount(root_part, TARGETDIR, "ext4", 0, NULL) != 0) {
-        log_to_ui(app, "ERROR mounting root.", 0.0); app->installing = FALSE; return NULL;
-    }
+    mount(root_part, TARGETDIR, "ext4", 0, NULL);
 
     if (app->is_efi) {
         run_sync(app, "mkdir -p %s/boot/efi", TARGETDIR);
         run_sync(app, "mount %s %s/boot/efi", efi_part, TARGETDIR);
     }
 
-    // 3. PREPARE XBPS ENVIRONMENT (The missing steps)
-    log_to_ui(app, "Configuring XBPS environment (Keys & Repos)...", 0.3);
-    run_sync(app, "mkdir -p %s/var/db/xbps/keys", TARGETDIR);
-    run_sync(app, "cp /var/db/xbps/keys/*.plist %s/var/db/xbps/keys/ 2>/dev/null", TARGETDIR);
+    // 3. COPY ROOTFS (The missing step)
+    log_to_ui(app, "Copying Live Image to Target (this will take time)...", 0.3);
+    // Using system() for the complex tar pipe. 
+    // Note: --one-file-system ensures we don't copy /mnt/target itself recursively.
+    // Note: 2>/dev/null is used to silence errors about changing permissions on special files which are expected.
+    int ret = system("tar -cf - --one-file-system --xattrs / 2>/dev/null | tar --extract --xattrs --xattrs-include='*' --preserve-permissions -f - -C /mnt/target");
     
-    run_sync(app, "mkdir -p %s/usr/share", TARGETDIR);
-    run_sync(app, "cp -r /usr/share/xbps.d %s/usr/share/", TARGETDIR);
-    
-    // Optional: copy mirrors if they were configured by the user (skipped for this simple installer)
-    // but we create the directory just in case.
-    run_sync(app, "mkdir -p %s/etc/xbps.d", TARGETDIR);
-
-    // 4. INSTALL PACKAGES (Replicating install_packages logic)
-    log_to_ui(app, "Installing base system packages (download + install)...", 0.4);
-    
-    // Select GRUB package
-    gchar *grub_pkg = "grub";
-    if (app->is_efi) {
-        grub_pkg = "grub-x86_64-efi"; // Simplified for x86_64
+    if (WEXITSTATUS(ret) != 0) {
+        log_to_ui(app, "ERROR: Failed to copy filesystem.", 0.0);
+        app->installing = FALSE; return NULL;
     }
 
-    // Run xbps-install. We use system() because handling the environment vars in C is verbose.
-    // This command includes the arch detection.
-    run_sync(app, "XBPS_ARCH=$(xbps-uhelper arch) xbps-install -Sy -r %s base-system %s", TARGETDIR, grub_pkg);
-
-    // 5. RECONFIGURE PACKAGES (The missing steps)
-    log_to_ui(app, "Reconfiguring base-files...", 0.6);
-    run_sync(app, "xbps-reconfigure -r %s -f base-files", TARGETDIR);
+    // 4. CLEANUP LIVE FILES (Remove installer traces)
+    log_to_ui(app, "Cleaning up live image files...", 0.4);
+    // Remove messages
+    run_sync(app, "rm -f %s/etc/motd", TARGETDIR);
+    run_sync(app, "rm -f %s/etc/issue", TARGETDIR);
+    // Remove installer binary (assuming standard path or self-cleaning)
+    run_sync(app, "rm -f %s/usr/sbin/void-installer", TARGETDIR);
+    // Remove SDDM config if live had autologin
+    run_sync(app, "rm -f %s/etc/sddm.conf", TARGETDIR);
     
-    log_to_ui(app, "Reconfiguring all packages (locales, initramfs)...", 0.7);
-    // This step might take a while (glibc-locales, dracut, etc)
-    run_sync(app, "chroot %s xbps-reconfigure -a", TARGETDIR);
+    // Remove Live User (Assuming live user is 'void' based on original script, or read /etc/passwd)
+    // For simplicity, we just remove the live user logic if it exists. 
+    // In a real scenario we would check /etc/default/live.conf
+    run_sync(app, "sed -i 's|GETTY_ARGS=\"--noclear -a void\"|GETTY_ARGS=\"--noclear\"|g' %s/etc/sv/agetty-tty1/conf", TARGETDIR);
 
-    // 6. System Settings
-    log_to_ui(app, "Applying configuration (Hostname, Locale)...", 0.8);
+    // 5. MOUNT DEV/PROC/SYS FOR CHROOT
+    log_to_ui(app, "Mounting virtual filesystems for configuration...", 0.5);
+    run_sync(app, "mount --rbind /dev %s/dev", TARGETDIR);
+    run_sync(app, "mount --rbind /proc %s/proc", TARGETDIR);
+    run_sync(app, "mount --rbind /sys %s/sys", TARGETDIR);
+
+    // 6. REBUILD INITRAMFS (Important for copy_rootfs flow)
+    log_to_ui(app, "Rebuilding initramfs...", 0.6);
+    // Dracut requires /etc/vconsole.conf or host keys sometimes, we rely on base-system config
+    run_sync(app, "chroot %s dracut --force --no-hostonly-cmdline", TARGETDIR);
+
+    // 7. REMOVE TEMPORARY PACKAGES
+    log_to_ui(app, "Removing temporary live packages...", 0.7);
+    run_sync(app, "chroot %s xbps-remove -Ry dialog xtools-minimal xmirror espeakup brltty 2>/dev/null", TARGETDIR);
+
+    // 8. CONFIGURATION (Hostname, Locale, Users)
+    log_to_ui(app, "Applying System Configuration...", 0.8);
     run_sync(app, "echo %s > %s/etc/hostname", hostname, TARGETDIR);
     
-    // Setup Locale
+    // Enable Locale
     run_sync(app, "sed -i 's/#%s/%s/' %s/etc/default/libc-locales", locale, locale, TARGETDIR);
     run_sync(app, "echo LANG=%s > %s/etc/locale.conf", locale, TARGETDIR);
-    // Reconfigure glibc-locales again to be sure
     run_sync(app, "chroot %s xbps-reconfigure -f glibc-locales", TARGETDIR);
 
-    // 7. Users
-    log_to_ui(app, "Configuring users...", 0.85);
+    // Root Password
     gchar *cmd_root = g_strdup_printf("echo 'root:%s' | chroot %s chpasswd", root_pass, TARGETDIR);
     system(cmd_root);
     g_free(cmd_root);
 
+    // User
     if (strlen(user_login) > 0) {
         run_sync(app, "chroot %s useradd -m -G wheel,audio,video -s /bin/bash %s", TARGETDIR, user_login);
         gchar *cmd_user = g_strdup_printf("echo '%s:%s' | chroot %s chpasswd", user_login, user_pass, TARGETDIR);
         system(cmd_user);
         g_free(cmd_user);
         
+        // Sudo setup
         run_sync(app, "echo '%%wheel ALL=(ALL:ALL) ALL' > %s/etc/sudoers.d/10-wheel", TARGETDIR);
         run_sync(app, "chmod 0440 %s/etc/sudoers.d/10-wheel", TARGETDIR);
     }
 
-    // 8. Bootloader
-    log_to_ui(app, "Installing GRUB...", 0.9);
+    // 9. BOOTLOADER (set_bootloader logic)
+    log_to_ui(app, "Installing GRUB Bootloader...", 0.9);
     if (app->is_efi) {
-        run_sync(app, "grub-install --target=x86_64-efi --efi-directory=%s/boot/efi --bootloader-id=void_grub --recheck %s", TARGETDIR, disk_path);
+        // EFI Install
+        run_sync(app, "chroot %s grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=void_grub --recheck %s", TARGETDIR, disk_path);
     } else {
-        run_sync(app, "grub-install --recheck %s", disk_path);
+        // MBR Install
+        run_sync(app, "chroot %s grub-install --recheck %s", TARGETDIR, disk_path);
     }
+    
+    // Generate Config
     run_sync(app, "chroot %s grub-mkconfig -o /boot/grub/grub.cfg", TARGETDIR);
 
+    // 10. UNMOUNT & FINALIZE
+    log_to_ui(app, "Unmounting filesystems...", 0.95);
+    run_sync(app, "umount -R %s", TARGETDIR);
+    
     log_to_ui(app, "--- INSTALLATION COMPLETED ---", 1.0);
     app->installing = FALSE;
     return NULL;

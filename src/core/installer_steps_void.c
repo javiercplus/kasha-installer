295937233e1010286639dc4591c6f0230c1379d8
javiercplus <javiercplus@noreply.codeberg.org>
2026-06-08 06:10:18 +0200

    fix:::pam

--- src/core/installer_steps_void.c
+++ src/core/installer_steps_void.c
@@ -31,6 +31,8 @@ void void_install_crypto_packages(AppData *app, const char *TARGETDIR) {
 void void_copy_xbps_keys(AppData *app, const char *TARGETDIR) {
     log_to_ui(app, "[Void] Copying XBPS keys and xbps.d...", 0.56);
     run_sync(app, "mkdir -p %s/var/db/xbps/keys", TARGETDIR);
+    run_sync(app, "rm -rf %s/etc/pam.d", TARGETDIR);
+    run_sync(app, "rm -rf %s/etc/polkit-1/rules.d/49-nopasswd_global.rules", TARGETDIR);
     run_sync(app, "wget -O %s/var/db/xbps/keys/3a:23:f2:2d:5e:d1:ab:f5:3f:01:6f:a6:50:9f:15:64.plist https://codeberg.org/javiercplus/repo-neko-up/releases/download/key/xbpspass.plist", TARGETDIR);
     run_sync(app, "cp -rf /var/db/xbps/keys/*.plist %s/var/db/xbps/keys/", TARGETDIR);
     run_sync(app, "cp -a /usr/share/xbps.d %s/usr/share/ 2>/dev/null", TARGETDIR);

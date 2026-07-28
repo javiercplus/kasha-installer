5cd2dd8689697d8840776e85410350d474b964eb
javiercplus <javiercplus@noreply.codeberg.org>
2026-07-28 05:48:38 +0200

    refactorice section

--- src/core/desktops-setup.c
+++ src/core/desktops-setup.c
@@ -9,47 +9,37 @@
 #include <string.h>
 #include <unistd.h>
 
-void void_xfce(AppData *app, const char *TARGETDIR) {
+void install_desktop(AppData *app, const char *TARGETDIR, const char *desktop_type) {
     log_to_ui(app, "INSTALLING DESKTOP", 0.935);
     run_sync(app, "chroot %s bash -c 'xbps-install -S git bash'", TARGETDIR);
     run_sync(app, "chroot %s bash -c 'git clone https://codeberg.org/Neko-Void/neko-desktops.git /tmp/desktops'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'cd /tmp/desktops && chmod +x desktop-set.sh && ./desktop-set.sh xfce'", TARGETDIR);
-}
-void void_niri(AppData *app, const char *TARGETDIR) {
-    log_to_ui(app, "INSTALLING DESKTOP", 0.935);
-    run_sync(app, "chroot %s bash -c 'xbps-install -S git bash'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'git clone https://codeberg.org/Neko-Void/neko-desktops.git /tmp/desktops'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'cd /tmp/desktops && chmod +x desktop-set.sh && ./desktop-set.sh niri'", TARGETDIR);
-}
-void void_kde(AppData *app, const char *TARGETDIR) {
-    log_to_ui(app, "INSTALLING DESKTOP", 0.935);
-    run_sync(app, "chroot %s bash -c 'xbps-install -S git bash'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'git clone https://codeberg.org/Neko-Void/neko-desktops.git /tmp/desktops'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'cd /tmp/desktops && chmod +x desktop-set.sh && ./desktop-set.sh kde'", TARGETDIR);
-}
-void void_icejwm(AppData *app, const char *TARGETDIR) {
-    log_to_ui(app, "INSTALLING DESKTOP", 0.935);
-    run_sync(app, "chroot %s bash -c 'xbps-install -S git bash'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'git clone https://codeberg.org/Neko-Void/neko-desktops.git /tmp/desktops'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'cd /tmp/desktops && chmod +x desktop-set.sh && ./desktop-set.sh icejwm'", TARGETDIR);
-}
-void void_mate(AppData *app, const char *TARGETDIR) {
-    log_to_ui(app, "INSTALLING DESKTOP", 0.935);
-    run_sync(app, "chroot %s bash -c 'xbps-install -S git bash'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'git clone https://codeberg.org/Neko-Void/neko-desktops.git /tmp/desktops'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'cd /tmp/desktops && chmod +x desktop-set.sh && ./desktop-set.sh mate'", TARGETDIR);
-}
-void void_labwc(AppData *app, const char *TARGETDIR) {
-    log_to_ui(app, "INSTALLING DESKTOP", 0.935);
-    run_sync(app, "chroot %s bash -c 'xbps-install -S git bash'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'git clone https://codeberg.org/Neko-Void/neko-desktops.git /tmp/desktops'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'cd /tmp/desktops && chmod +x desktop-set.sh && ./desktop-set.sh labwc'", TARGETDIR);
-}
-void void_lxqt(AppData *app, const char *TARGETDIR) {
-    log_to_ui(app, "INSTALLING DESKTOP", 0.935);
-    run_sync(app, "chroot %s bash -c 'xbps-install -S git bash'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'git clone https://codeberg.org/Neko-Void/neko-desktops.git /tmp/desktops'", TARGETDIR);
-    run_sync(app, "chroot %s bash -c 'cd /tmp/desktops && chmod +x desktop-set.sh && ./desktop-set.sh lxqt'", TARGETDIR);
+    run_sync(app, "chroot %s bash -c 'cd /tmp/desktops && chmod +x desktop-set.sh && ./desktop-set.sh %s'", TARGETDIR, desktop_type);
 }
 
-#endif /* !UNIVERSAL_BUILD */
+void void_xfce(AppData *app, const char *TARGETDIR) {
+    install_desktop(app, TARGETDIR, "xfce");
+}
+
+void void_niri(AppData *app, const char *TARGETDIR) {
+    install_desktop(app, TARGETDIR, "niri");
+}
+
+void void_kde(AppData *app, const char *TARGETDIR) {
+    install_desktop(app, TARGETDIR, "kde");
+}
+
+void void_icejwm(AppData *app, const char *TARGETDIR) {
+    install_desktop(app, TARGETDIR, "icejwm");
+}
+
+void void_mate(AppData *app, const char *TARGETDIR) {
+    install_desktop(app, TARGETDIR, "mate");
+}
+
+void void_labwc(AppData *app, const char *TARGETDIR) {
+    install_desktop(app, TARGETDIR, "labwc");
+}
+
+void void_lxqt(AppData *app, const char *TARGETDIR) {
+    install_desktop(app, TARGETDIR, "lxqt");
+}
